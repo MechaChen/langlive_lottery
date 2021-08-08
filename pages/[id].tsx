ad4987204802ab270ecda592e9750d6c261ac243
Benson <tumlivein@gmail.com>
2021-08-08 20:00:51 +0800

    feature: 在結果頁新增返回按鈕

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -2,10 +2,13 @@ import { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '@redux/store'
 import { I_Drawer } from '@redux/reducer'
+import { useRouter } from 'next/router'
+import * as Styled from './styles/owner'
 
 function Owner() {
     const { List } = useSelector((state: RootState) => state)
     const [owner, setOwner] = useState<I_Drawer>({ id: '', name: '' })
+    const router = useRouter()
 
     useEffect(() => {
         const id = window?.location.pathname.replace('/', '')
@@ -13,11 +16,16 @@ function Owner() {
         setOwner(owner)
     }, [])
 
+    const backToHome = () => {
+        router.push('/')
+    }
+
     return (
-        <div>
+        <Styled.Container>
             <h3>抽獎結果</h3>
             <div>{owner.name}</div>
-        </div>
+            <Styled.Button onClick={backToHome}>返回抽獎頁</Styled.Button>
+        </Styled.Container>
     )
 }
 
---
+++ pages/styles/owner.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    max-width: 980px;
+    padding: 0 10px;
+    box-sizing: border-box;
+`
+export const Button = styled.button`
+    margin-top: 30px;
+    width: 100px;
+    line-height: 40px;
+    color: #26bec9;
+    box-sizing: border-box;
+    border-radius: 4px;
+    box-shadow: 0 5px 10px rgb(0 0 0 / 10%);
+    border: 1px solid #26bec9;
+    background-color: #fff;
+    cursor: pointer;
+`
